062fcf6401c0626d0d2f31dd4dd5aa77cf91fb48
Carlos Toro <carlos.toro@wnco.com>
2020-04-02 14:37:09 -0500

    Update VPC id

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo docker rm $(docker stop $(docker ps -a -q --filter ancestor=cmtoro0/carlos_nginx:latest --format="{{.ID}}"))
+sudo docker rm $(sudo docker stop $(sudo docker ps -a -q --filter ancestor=cmtoro0/carlos_nginx:latest --format="{{.ID}}"))
 sudo docker image rm cmtoro0/carlos_nginx:latest
 sudo docker run -d -p80:80 cmtoro0/carlos_nginx:latest
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,6 +1,6 @@
 project_name = "my-terraform"
 
-vpc_id = "vpc-b0ea91d7"
+vpc_id = "vpc-6a232f0d"
 
 ami_id = "ami-05494b93950efa2fd"
 
